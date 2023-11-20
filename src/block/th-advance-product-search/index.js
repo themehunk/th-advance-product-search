9c2ba133c30b8e6ffc60e4fafe345635f69cabd7
shivam kushwah <37370154+devkushwahshivam@users.noreply.github.com>
2023-11-20 16:17:17 +0530

    search block added

---
+++ src/block/th-advance-product-search/index.js
@@ -0,0 +1,35 @@
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * All files containing `style` keyword are bundled together. The code used
+ * gets applied both to the front of your site and to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './style.scss';
+
+/**
+ * Internal dependencies
+ */
+import edit from './edit';
+//import save from './save';
+import metadata from './block.json';
+
+import { RxMagnifyingGlass as icon } from "react-icons/rx";
+
+const { name } = metadata;
+
+registerBlockType( name, {
+	...metadata,
+	title: __( 'TH Advance Product Search', 'th-advance-product-search' ),
+	description: __( 'Fastest search plugin for WooCommerce. Supports AJAX search and search results page display', 'th-advance-product-search' ),
+	icon,
+	edit,
+	save: () => null,
+});
---
+++ src/block/th-advance-product-search/settings.js
@@ -0,0 +1,74 @@
+import { __ } from '@wordpress/i18n';
+import {AlignmentToolbar, __experimentalColorGradientControl as ColorGradientControl,InspectorControls} from '@wordpress/block-editor';
+
+import {
+    PanelBody,
+    RangeControl,
+    SelectControl,
+    Placeholder,
+    Spinner,
+    ToggleControl,
+} from '@wordpress/components';
+
+import { useSelect } from '@wordpress/data';
+import {
+	RichText
+} from '@wordpress/block-editor';
+
+import {
+    Fragment,
+    useState,
+    Suspense
+} from '@wordpress/element';
+
+const InsSettings = ({
+    attributes,
+    setAttributes
+}) => {
+
+    const adminUrlsearch = ThBlockData.adminUrlsearch;
+
+    return (<Fragment>
+        <InspectorControls>
+        <PanelBody
+					title={ __( 'TH Advance Search Setting', 'vayu-blocks' ) }
+					initialOpen={true}
+					className="th-sEARCH-panel"
+				>   
+        <SelectControl
+								label={ __( 'Search Style', 'th-advance-product-search' ) }
+								value={ attributes.searchStyle }
+								options={ [
+									{ label: __( 'Default', 'th-advance-product-search' ), value: '[th-aps]' },
+									{ label: __( 'bar-style', 'th-advance-product-search' ), value: '[th-aps layout="bar_style"]' },
+									{ label: __( 'icon-style', 'th-advance-product-search' ), value: '[th-aps layout="icon_style"]' },
+								    { label: __( 'flexible-style', 'th-advance-product-search' ), value: '[th-aps layout="flexible-style"]' },
+								] }
+								onChange={ e => setAttributes({ searchStyle: e }) }
+							/>
+                             <p>
+                        {__(
+                            'For the ',
+                            'th-advance-product-search'
+                        )}
+                        <strong>{__('Advanced Options and Styling', 'th-advance-product-search')}</strong>
+                           
+                        {__(
+                            ' navigate to ',
+                            'th-advance-product-search'
+                        )}
+                        <a
+                            href={adminUrlsearch}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {__('Settings', 'th-advance-product-search')}
+                        </a>
+                    
+                    </p>
+                            </PanelBody>
+        </InspectorControls>
+    </Fragment>)   
+}
+
+export default InsSettings;
