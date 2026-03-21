02409314b3abc3d9557d98f8009eac707c1430fc
ThemeHunk <themehunk@gmail.com>
2026-03-21 11:39:26 +0530

    style issue fixed

--- inc/thaps-premium.php
+++ inc/thaps-premium.php
@@ -6,22 +6,13 @@
     <h2 class="th-premium-title">
         <?php esc_html_e( 'Unlock This Feature with Premium', 'th-all-in-one-woo-cart' ); ?>
            <span class="th-premium-badge">
-        <?php esc_html_e( 'Premium Feature', 'th-all-in-one-woo-cart' ); ?>
+        <a href="<?php echo esc_url( 'https://themehunk.com/advance-product-search/' ); ?>" target="_blank" rel="noopener noreferrer">
+            <?php esc_html_e( 'Premium Feature', 'th-all-in-one-woo-cart' ); ?>
+        </a>
         </span>
     </h2>
 
     <p class="th-premium-desc">
         <?php esc_html_e( 'These options are available for preview purposes only. You can explore and test them here, but they will not be applied to the live frontend of the website. To activate these advanced features on your site, please upgrade to the Pro version.', 'th-all-in-one-woo-cart' ); ?>
     </p>
-
-    <div class="th-premium-actions">
-        <a href="<?php echo esc_url( 'https://themehunk.com/advance-product-search/' ); ?>"
-           class="th-premium-btn"
-           target="_blank"
-           rel="noopener noreferrer">
-            <?php esc_html_e( 'Go Premium', 'th-all-in-one-woo-cart' ); ?>
-        </a>
-
-    </div>
-
 </div>
