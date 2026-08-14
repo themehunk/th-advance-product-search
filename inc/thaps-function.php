17887181dc61eca59cb7e6ec3ee075b8067847b3
iheeresh <heereshd@gmail.com>
2026-08-14 16:09:39 +0530

    ajax same name

--- inc/thaps-function.php
+++ inc/thaps-function.php
@@ -32,8 +32,8 @@ if ( ! class_exists( 'TH_Advancde_Product_Search_Functions' ) ):
 		public function __construct(){
 
           add_filter( 'posts_search',   [ $this, 'modify_search_sql' ], 501, 2 );
-           add_action( 'wp_ajax_thaps_ajax_get_search_value',array( $this, 'thaps_ajax_get_search_value' ));
-           add_action( 'wp_ajax_nopriv_thaps_ajax_get_search_value',array( $this, 'thaps_ajax_get_search_value' ));
+           add_action( 'wp_ajax_thaps_ajax_get_search_value',array( $this, 'thaps_ajax_get_search_value_call' ));
+           add_action( 'wp_ajax_nopriv_thaps_ajax_get_search_value',array( $this, 'thaps_ajax_get_search_value_call' ));
 
 
 		}
@@ -541,7 +541,7 @@ if ( ! class_exists( 'TH_Advancde_Product_Search_Functions' ) ):
 	/*************************/
     // search result function
 	/*************************/
-		public function thaps_ajax_get_search_value(){
+		public function thaps_ajax_get_search_value_call(){
 
         check_ajax_referer( 'th_advance_product_search', 'nonce' );
 
